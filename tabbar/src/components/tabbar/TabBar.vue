4d5a6c083aecbf4b5abb3f776f145f6c2205b7ac
nienie <765891612@qq.com>
2020-07-10 00:01:25 +0800

    custom a tab component using vue

---
+++ tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  box-shadow: 0 -1px 1px rgba(0, 0, 0, .2);
+}
+</style>
---
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <slot v-if="isActive" name="item-icon-active"></slot>
+    <slot v-else name="item-icon"></slot>
+    <div :style="activeStyle">
+      <slot name="item-title"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'ff00f0'
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.path);
+    }
+  }
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-bottom: 2px;
+  vertical-align: middle;
+}
+.active {
+  color: #f0f;
+}
+</style>
